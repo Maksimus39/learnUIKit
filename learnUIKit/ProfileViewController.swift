e53a54dc322398561979ddfb9dd333d9ca1522b2
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-11-14 14:58:09 -0800

    lesson 1

---
+++ learnUIKit/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  learnUIKit
+//
+//  Created by Максим Минаков on 14.11.2025.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
