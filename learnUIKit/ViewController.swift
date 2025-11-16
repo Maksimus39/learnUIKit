96d97b32efa5d5b5bfbe53e6a7f27e39bb0dab93
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-11-16 09:30:31 -0800

    homeWork module 2 lesson 1

--- learnUIKit/ViewController.swift
+++ learnUIKit/ViewController.swift
@@ -1,50 +1,79 @@
-//
-//  ViewController.swift
-//  learnUIKit
-//
-//  Created by Максим Минаков on 14.11.2025.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        print("Hello")
         
-        view.backgroundColor = .orange
-        // label
-        let label = UILabel()
-        label.text = "Hello world"
-        label.frame = CGRect(x: 100, y: 100, width: 300, height: 50)
-        label.textColor = .blue
-        label.font = .systemFont(ofSize: 50)
-        view.addSubview(label)
-        print(view.frame)
+        // UIImageView
+        let avatarImageView = UIImageView()
+        avatarImageView.image = UIImage(named: "img1")
+        avatarImageView.frame = CGRect(x: 36, y: 60, width: 114, height: 114)
+        view.addSubview(avatarImageView)
         
-        // image
-        let someImageView = UIImageView()
-        someImageView.frame = CGRect(x: 100, y: 200, width: 300, height: 300)
-        someImageView.backgroundColor = .green
-        someImageView.image = UIImage(named: "img1")
-        someImageView.contentMode = .scaleAspectFill
-        view.addSubview(someImageView)
+        // UILabel name surname
+        let nameSurnameLabel = UILabel()
+        nameSurnameLabel.text = "Имя Фамилия"
+        nameSurnameLabel.frame = CGRect(x: 164, y: 82, width: 109, height: 19)
+        nameSurnameLabel.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        nameSurnameLabel.textColor = .label
+        view.addSubview(nameSurnameLabel)
         
-        // button
-        let action = UIAction { _ in
-            someImageView.frame = self.view.frame
-        }
+        // UILabel age
+        let ageLabel = UILabel()
+        ageLabel.text = "30 лет"
+        ageLabel.frame = CGRect(x: 164, y: 113, width: 52, height: 19)
+        ageLabel.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        ageLabel.textColor = .label
+        view.addSubview(ageLabel)
         
-        let button = UIButton(frame: CGRect(x: 50,
-                                            y: 600,
-                                            width: 200,
-                                            height: 80), primaryAction: action)
+        // UIButton
+        let button = UIButton()
+        button.frame = CGRect(x: 36, y: 225, width: 321, height: 53)
+        button.backgroundColor = .black
         
-        button.setTitle("btn", for: .normal)
-        button.backgroundColor = .green
+        // Установка текста кнопки
+        button.setTitle("Редактировать профиль", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .regular)
         view.addSubview(button)
+        
+        // UILabel
+        let aboutMe = UILabel()
+        aboutMe.text = "О себе"
+        aboutMe.frame = CGRect(x: 36, y: 316, width: 54, height: 19)
+        aboutMe.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        aboutMe.textColor = .black
+        view.addSubview(aboutMe)
+        
+        // UILabel
+        let aboutMeText = UILabel()
+        aboutMeText.text = """
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            """
+        aboutMeText.frame = CGRect(x: 36, y: 346, width: 321, height: 114)
+        aboutMeText.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        aboutMeText.textColor = .black
+        aboutMeText.numberOfLines = 0
+        aboutMeText.lineBreakMode = .byWordWrapping
+        view.addSubview(aboutMeText)
+        
+        // UIButton
+        let buttonDeleteData = UIButton()
+        buttonDeleteData.frame = CGRect(x: 36, y: 498, width: 321, height: 53)
+        buttonDeleteData.backgroundColor = .red
+        
+        // Установка текста кнопки
+        buttonDeleteData.setTitle("Удалить данные", for: .normal)
+        buttonDeleteData.setTitleColor(.white, for: .normal)
+        buttonDeleteData.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        view.addSubview(buttonDeleteData)
+        
+        // UIImageView
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "img2")
+        imageView.frame = CGRect(x: 36, y: 589, width: 321, height: 185)
+        view.addSubview(imageView)
     }
 }
 
