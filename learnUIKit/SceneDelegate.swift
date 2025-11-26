1c12e5c0df89fc2a221e5acfe2de63845e6706a1
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-11-26 04:28:39 -0800

    module2lesson3NavigationHomeWork

--- learnUIKit/SceneDelegate.swift
+++ learnUIKit/SceneDelegate.swift
@@ -14,16 +14,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
-        //1
         guard let scene = (scene as? UIWindowScene) else { return }
         
-        //2
         self.window = UIWindow(windowScene: scene)
         
-        //3
         self.window?.rootViewController = UINavigationController(rootViewController: ViewController())
         
-        //4
         self.window?.makeKeyAndVisible()
     }
 }
