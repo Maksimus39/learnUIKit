bb342794a91f73b5e62be2bf8153ded366424a5c
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-11-26 04:27:47 -0800

    module2lesson3NavigationHomeWork

--- learnUIKit/SettingsViewController.swift
+++ learnUIKit/SettingsViewController.swift
@@ -3,53 +3,133 @@ import UIKit
 
 class SettingsViewController: UIViewController {
     
-    lazy  var action: UIAction = UIAction { [weak self] _ in
-        //1
-        //let viewController = ViewController()
-        
-        //2
+    lazy var firstNameUser = createLabel(textLabel: "first name", textSize: 14)
+    lazy var textFieldFirstName = createTextField(placeholder: "Enter your first name", textSize: 16)
+    lazy var lastNameUser = createLabel(textLabel: "last name", textSize: 14)
+    lazy var textFieldLastName = createTextField(placeholder: "Enter your last name", textSize: 16)
+    lazy var nationaldUser = createLabel(textLabel: "National ID number", textSize: 14)
+    lazy var textFieldIDNumber = createTextField(placeholder: "Enter your National ID number", textSize: 16)
+    
+    lazy var settingsAction: UIAction = UIAction { [weak self] _ in
         self?.navigationController?.popViewController(animated: true)
     }
     
-    lazy  var btn: UIButton = {
-        $0.setTitle("back to home", for: .normal)
+    lazy var mainSettingsButton: UIButton = {
+        $0.setImage(UIImage(systemName: "chevron.left"), for: .normal)
+        $0.setTitle("Главная", for: .normal)
         $0.translatesAutoresizingMaskIntoConstraints = false
-        
         return $0
-    }(UIButton(primaryAction: action))
-    
-    override func viewWillAppear(_ animated: Bool) {
-        print("2 - viewWillAppear")
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        print("2 - viewDidAppear")
-    }
+    }(UIButton(primaryAction: settingsAction))
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("2 - viewDidLoad")
-        view.backgroundColor = .brown
-        title = "back to home"
+        view.backgroundColor = .white
+        title = "Настройки"
         
-        view.addSubview(btn)
+        view.addSubview(mainSettingsButton)
+        view.addSubview(firstNameUser)
+        view.addSubview(textFieldFirstName)
+        view.addSubview(lastNameUser)
+        view.addSubview(textFieldLastName)
+        view.addSubview(nationaldUser)
+        view.addSubview(textFieldIDNumber)
+        view.addSubview(createButton)
         
+        
+        
+        createSettingsConstraint()
+        
+        
+        
+        let buttonItem = UIBarButtonItem(customView: mainSettingsButton)
+        navigationItem.leftBarButtonItem = buttonItem
+    }
+    
+    var createButton:UIButton = {
+        $0.setTitle("сохранить", for: .normal)
+        $0.backgroundColor = UIColor(named: "figmaGreen")
+        $0.setTitleColor(.white, for: .normal)
+        $0.titleLabel?.font = .systemFont(ofSize: 16)
+        $0.layer.cornerRadius = 16
+        $0.contentHorizontalAlignment = .center
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UIButton())
+    
+    private func createLabel(textLabel: String, textSize: Int) -> UILabel {
+        let uiLabel = UILabel()
+        uiLabel.text = textLabel
+        uiLabel.textColor = .black
+        uiLabel.font = .systemFont(ofSize: CGFloat(textSize))
+        uiLabel.translatesAutoresizingMaskIntoConstraints = false
+        return uiLabel
+    }
+    
+    private func createTextField(placeholder: String, textSize: Int, height: CGFloat = 48) -> UITextField {
+        let textField = UITextField()
+        textField.placeholder = placeholder
+        textField.textColor = .black
+        textField.backgroundColor = UIColor(named: "LightGrayBackground")
+        textField.layer.cornerRadius = 10
+        textField.font = .systemFont(ofSize: CGFloat(textSize))
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.heightAnchor.constraint(equalToConstant: height).isActive = true
+        textField.leftView = UIView(frame:  CGRect(x: 0, y: 0, width: 16, height: 0))
+        textField.leftViewMode = .always
+        textField.rightView = UIView(frame:  CGRect(x: 0, y: 0, width: 16, height: 0))
+        textField.rightViewMode = .always
+        return textField
+    }
+    
+    private func createSettingsConstraint(){
         NSLayoutConstraint.activate([
-            btn.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            btn.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            // constraint firstNameUser
+            firstNameUser.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
+            firstNameUser.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25),
+            firstNameUser.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            firstNameUser.heightAnchor.constraint(equalToConstant: 20),
+            
+            // constraint textFieldFirstName
+            textFieldFirstName.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
+            textFieldFirstName.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25),
+            textFieldFirstName.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 40),
+            textFieldFirstName.heightAnchor.constraint(equalToConstant: 48),
+            
+            // constraint lastNameUser
+            lastNameUser.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
+            lastNameUser.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25),
+            lastNameUser.topAnchor.constraint(equalTo: textFieldFirstName.bottomAnchor, constant: 16),
+            lastNameUser.heightAnchor.constraint(equalToConstant: 20),
+            
+            // constraint textFieldLastName
+            textFieldLastName.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
+            textFieldLastName.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25),
+            textFieldLastName.topAnchor.constraint(equalTo: lastNameUser.bottomAnchor, constant: 4),
+            textFieldLastName.heightAnchor.constraint(equalToConstant: 48),
+            
+            // constraint nationaldUser
+            nationaldUser.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
+            nationaldUser.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25),
+            nationaldUser.topAnchor.constraint(equalTo: textFieldLastName.bottomAnchor, constant: 16),
+            nationaldUser.heightAnchor.constraint(equalToConstant: 20),
+            
+            // constraint textFieldIDNumber
+            textFieldIDNumber.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
+            textFieldIDNumber.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25),
+            textFieldIDNumber.topAnchor.constraint(equalTo: nationaldUser.bottomAnchor, constant: 4),
+            textFieldIDNumber.heightAnchor.constraint(equalToConstant: 48),
+            
+            // constraint createButton
+            createButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
+            createButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25),
+            createButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -48),
+            createButton.heightAnchor.constraint(equalToConstant: 48),
+            
         ])
     }
     
     deinit {
-        print("close settings")
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        print("2 - viewWillDisappear")
-    }
-    
-    override func viewDidDisappear(_ animated: Bool) {
-        print("2 - viewDidDisappear")
+        print("SettingsViewController deinit")
     }
 }
--- learnUIKit/ViewController.swift
+++ learnUIKit/ViewController.swift
@@ -2,59 +2,129 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    lazy  var action: UIAction = UIAction { [weak self] _ in
-        //1
+    lazy var nameSurnameUser = createLabel(textLabel: "User Name", textSize: 20, weight: UIFont.Weight(rawValue: 900))
+    lazy var yearsUser = createLabel(textLabel: "20 years", textSize: 14, weight: UIFont.Weight(500))
+    lazy var aboutUser = createLabel(textLabel: "About", textSize: 14, weight: UIFont.Weight(900))
+    
+    
+    lazy var action: UIAction = UIAction { [weak self] _ in
         let settingView = SettingsViewController()
-        
-        //2
         self?.navigationController?.pushViewController(settingView, animated: true)
     }
     
-    lazy  var btn: UIButton = {
-        $0.setTitle("Settings", for: .normal)
+    lazy var mainButton: UIButton = {
+        $0.setImage(UIImage(systemName: "gear"), for: .normal)
         $0.translatesAutoresizingMaskIntoConstraints = false
-        
         return $0
     }(UIButton(primaryAction: action))
     
-    override func viewWillAppear(_ animated: Bool) {
-        print("1 - viewWillAppear")
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        print("1 - viewDidAppear")
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("1 - viewDidLoad")
-        view.backgroundColor = .orange
-        title = "Hello world"
-        navigationController?.navigationBar.prefersLargeTitles = true
+        print("homeWork - 3")
+        view.backgroundColor = .white
+        title = "Главная"
         
-        view.addSubview(btn)
+        view.addSubview(mainButton)
+        view.addSubview(imageView)
+        view.addSubview(nameSurnameUser)
+        view.addSubview(yearsUser)
+        view.addSubview(aboutUser)
+        view.addSubview(createTextView)
+        view.addSubview(createButton)
         
+        createRootConstraint()
+        
+        let buttonItem = UIBarButtonItem(customView: mainButton)
+        navigationItem.rightBarButtonItem = buttonItem
+    }
+    
+    var imageView: UIImageView = {
+        $0.image = UIImage(named: "img")
+        $0.contentMode = .scaleAspectFill
+        $0.layer.cornerRadius = 30
+        $0.clipsToBounds = true
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        
+        return $0
+    }(UIImageView())
+    
+    var createTextView: UITextView = {
+        $0.text = """
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
+            """
+        $0.textColor = .black
+        $0.backgroundColor = .white
+        $0.font = .systemFont(ofSize: 14)
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.isScrollEnabled = true
+        return $0
+    }(UITextView())
+    
+    var createButton:UIButton = {
+        $0.setTitle("сохранить", for: .normal)
+        $0.backgroundColor = UIColor(named: "figmaGreen")
+        $0.setTitleColor(.white, for: .normal)
+        $0.titleLabel?.font = .systemFont(ofSize: 16)
+        $0.layer.cornerRadius = 16
+        $0.contentHorizontalAlignment = .center
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UIButton())
+    
+    private func createLabel(textLabel: String, textSize: Int, weight: UIFont.Weight) -> UILabel {
+        let uiLabel = UILabel()
+        uiLabel.text = textLabel
+        uiLabel.textColor = .black
+        uiLabel.font = .systemFont(ofSize: CGFloat(textSize), weight: weight)
+        uiLabel.translatesAutoresizingMaskIntoConstraints = false
+        return uiLabel
+    }
+    
+    private func createRootConstraint() {
         NSLayoutConstraint.activate([
-            btn.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            btn.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            // constraint imageView
+            imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
+            imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -268),
+            imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 12),
+            imageView.heightAnchor.constraint(equalToConstant: 100),
+            
+            // constraint uilabel nameSurnameUser
+            nameSurnameUser.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 139),
+            nameSurnameUser.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25),
+            nameSurnameUser.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 40),
+            nameSurnameUser.heightAnchor.constraint(equalToConstant: 24),
+            
+            // constraint uilabel yearsUser
+            yearsUser.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 139),
+            yearsUser.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25),
+            yearsUser.topAnchor.constraint(equalTo: nameSurnameUser.bottomAnchor, constant: 3),
+            yearsUser.heightAnchor.constraint(equalToConstant: 17),
+            
+            // constraint uilabel aboutUser
+            aboutUser.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
+            aboutUser.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25),
+            aboutUser.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 35),
+            aboutUser.heightAnchor.constraint(equalToConstant: 17),
+            
+            // constraint uilabel createTextView
+            createTextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
+            createTextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25),
+            createTextView.topAnchor.constraint(equalTo: aboutUser.bottomAnchor, constant: 11),
+            createTextView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            
+            // constraint uilabel button
+            createButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
+            createButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25),
+            createButton.topAnchor.constraint(equalTo: yearsUser.bottomAnchor, constant: 227),
+            createButton.heightAnchor.constraint(equalToConstant: 48),
         ])
-        
-        // leftBarButtonItem - если слева отобразить а можно и массив кнопок
-        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "camera"), style: .plain, target: self, action: #selector(setCameraBtn))
     }
     
-    @objc func setCameraBtn(){
-        let settingView = SettingsViewController()
-        
-        self.present(settingView, animated: true)
-        print("select camera")
-    }
     
-    override func viewWillDisappear(_ animated: Bool) {
-        print("1 - viewWillDisappear")
-    }
     
-    override func viewDidDisappear(_ animated: Bool) {
-        print("1 - viewDidDisappear")
+    deinit {
+        print("ViewController deinit")
     }
 }
