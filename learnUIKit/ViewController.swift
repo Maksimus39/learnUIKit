ced76c044be3af43f064c7a311cd96a5ade6fc61
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-11-28 21:26:41 -0800

    lesson_4

--- learnUIKit/ViewController.swift
+++ learnUIKit/ViewController.swift
@@ -1,50 +1,82 @@
-//
-//  ViewController.swift
-//  learnUIKit
-//
-//  Created by Максим Минаков on 14.11.2025.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private lazy var bunnerView: UIView = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.backgroundColor = .lightGray
+        $0.layer.cornerRadius = 10
+        return $0
+    }(UIView())
+    
+    
+    private lazy var imageView: UIImageView = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.image = UIImage(named: "imgOleg")
+        $0.layer.cornerRadius = 10
+        $0.clipsToBounds = true
+        $0.contentMode = .scaleAspectFill
+        return $0
+    }(UIImageView())
+    
+    
+    private lazy var leftButton: UIButton = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        //$0.backgroundColor = .red
+        $0.setImage(.imgR, for: .normal)
+        $0.tintColor = .white
+        return $0
+    }(UIButton())
+    
+    private lazy var nameLabel: UILabel = {
+        $0.text = """
+            qwertyuiopasdfghjkl.,mnbvcxzvhjkdgchjvjfhvjkfhjvkbfnsjkvbnfjskbbvjkvjkbsdfnjkvbfgjb jfgs
+            qwertyuiopasdfghjkl.,mnbvcxzvhjkdgchjvjfhvjkfhjvkbfnsjkvbnfjskbbvjkvjkbsdfnjkvbfgjb jfgs
+            """
+        $0.font = .systemFont(ofSize: 30, weight: .bold)
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.textColor = .blue
+        $0.numberOfLines = 0
+        return $0
+    }(UILabel())
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        print("Hello")
+        view.backgroundColor = .white
         
-        view.backgroundColor = .orange
-        // label
-        let label = UILabel()
-        label.text = "Hello world"
-        label.frame = CGRect(x: 100, y: 100, width: 300, height: 50)
-        label.textColor = .blue
-        label.font = .systemFont(ofSize: 50)
-        view.addSubview(label)
-        print(view.frame)
+        view.addSubview(bunnerView)
+        bunnerView.addSubview(imageView)
+        bunnerView.addSubview(leftButton)
+        bunnerView.addSubview(nameLabel)
         
-        // image
-        let someImageView = UIImageView()
-        someImageView.frame = CGRect(x: 100, y: 200, width: 300, height: 300)
-        someImageView.backgroundColor = .green
-        someImageView.image = UIImage(named: "img1")
-        someImageView.contentMode = .scaleAspectFill
-        view.addSubview(someImageView)
-        
-        // button
-        let action = UIAction { _ in
-            someImageView.frame = self.view.frame
-        }
-        
-        let button = UIButton(frame: CGRect(x: 50,
-                                            y: 600,
-                                            width: 200,
-                                            height: 80), primaryAction: action)
-        
-        button.setTitle("btn", for: .normal)
-        button.backgroundColor = .green
-        view.addSubview(button)
+        NSLayoutConstraint.activate([
+            // constraint bunnerView
+            bunnerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            bunnerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            bunnerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            //bunnerView.heightAnchor.constraint(equalToConstant: 100), // ❌ Этого нет
+            
+            // constraint imageView
+            imageView.topAnchor.constraint(equalTo: bunnerView.topAnchor, constant: 10),
+            imageView.leadingAnchor.constraint(equalTo: bunnerView.leadingAnchor, constant: 10),
+            imageView.widthAnchor.constraint(equalToConstant: 50),
+            imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: 1),
+            
+            // constraint leftButton
+            leftButton.topAnchor.constraint(equalTo: bunnerView.topAnchor, constant: 10),
+            leftButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            leftButton.widthAnchor.constraint(equalToConstant: 20),
+            leftButton.heightAnchor.constraint(equalToConstant: 20),
+            
+            // constraint imageView
+            //imageView.bottomAnchor.constraint(equalTo: bunnerView.bottomAnchor, constant: -10),
+            
+            // constraint nameLabel
+            nameLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 20),
+            nameLabel.leadingAnchor.constraint(equalTo: bunnerView.leadingAnchor, constant: 20),
+            nameLabel.trailingAnchor.constraint(equalTo: bunnerView.trailingAnchor, constant: -20),
+            nameLabel.bottomAnchor.constraint(equalTo: bunnerView.bottomAnchor, constant: -10)
+        ])
     }
+    
 }
-
