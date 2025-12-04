e24311f09cf01376186e0d525f46a18cfeda1a38
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-12-03 22:17:02 -0800

    module2lesson5

--- learnUIKit/ViewController.swift
+++ learnUIKit/ViewController.swift
@@ -1,50 +1,40 @@
-//
-//  ViewController.swift
-//  learnUIKit
-//
-//  Created by Максим Минаков on 14.11.2025.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private lazy var users: [String] = [
+        "USer-1",
+        "USer-2",
+        "USer-3",
+        "USer-4",
+        "USer-5",
+        "USer-6",
+        "USer-7",
+        "USer-8",
+    ]
+    
+    private lazy var tableView: UITableView = {
+        // 1 регистрация ячейки
+        $0.register(UITableViewCell.self, forCellReuseIdentifier: "userCell")
+        $0.dataSource = self
+        return $0
+    }(UITableView(frame: view.frame, style: .insetGrouped))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        print("Hello")
-        
-        view.backgroundColor = .orange
-        // label
-        let label = UILabel()
-        label.text = "Hello world"
-        label.frame = CGRect(x: 100, y: 100, width: 300, height: 50)
-        label.textColor = .blue
-        label.font = .systemFont(ofSize: 50)
-        view.addSubview(label)
-        print(view.frame)
-        
-        // image
-        let someImageView = UIImageView()
-        someImageView.frame = CGRect(x: 100, y: 200, width: 300, height: 300)
-        someImageView.backgroundColor = .green
-        someImageView.image = UIImage(named: "img1")
-        someImageView.contentMode = .scaleAspectFill
-        view.addSubview(someImageView)
-        
-        // button
-        let action = UIAction { _ in
-            someImageView.frame = self.view.frame
-        }
-        
-        let button = UIButton(frame: CGRect(x: 50,
-                                            y: 600,
-                                            width: 200,
-                                            height: 80), primaryAction: action)
-        
-        button.setTitle("btn", for: .normal)
-        button.backgroundColor = .green
-        view.addSubview(button)
+        view.backgroundColor = .opaqueSeparator
+        view.addSubview(tableView)
     }
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        users.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "userCell", for: indexPath)
+        cell.textLabel?.text = users[indexPath.row]
+        return cell
+    }
+}
