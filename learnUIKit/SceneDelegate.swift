3ad178f2558059bf7354374c02beea6a8d7fdcf3
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-12-22 13:58:21 -0800

    model data

--- learnUIKit/SceneDelegate.swift
+++ learnUIKit/SceneDelegate.swift
@@ -1,16 +1,13 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-    
     var window: UIWindow?
     
-    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let scene = (scene as? UIWindowScene) else { return }
         
+        guard let scene = (scene as? UIWindowScene) else {return}
         self.window = UIWindow(windowScene: scene)
         self.window?.rootViewController = UINavigationController(rootViewController: ViewController())
         self.window?.makeKeyAndVisible()
     }
 }
-
