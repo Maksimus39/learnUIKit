b24643a4a909c19fb4ce8badd795f1f2f91cbd20
Maksim Minakov <maksim.minakov.83@mail.ru>
2026-01-03 17:11:02 -0800

    imiitial commit

---
+++ learnUIKit/CollectionSection.swift
@@ -0,0 +1,52 @@
+import Foundation
+
+struct CollectionSection: Identifiable {
+    let id: String = UUID().uuidString
+    let items: [CollectionItem]
+    
+    static func mockdata() -> [CollectionSection] {
+        
+        let storyItems = [
+            CollectionItem(image: "img1" , name: "Name1"),
+            CollectionItem(image: "img2" , name: "Name1"),
+            CollectionItem(image: "img3" , name: "Name1"),
+            CollectionItem(image: "img4" , name: "Name1"),
+        ]
+        
+        let messageItems = [
+            CollectionItem(image: "img1", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit"),
+            CollectionItem(image: "img2", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit"),
+            CollectionItem(image: "img3", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit"),
+            CollectionItem(image: "img4", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit"),
+            CollectionItem(image: "img5", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit"),
+        ]
+        
+        let newsItems = [
+            CollectionItem(image: "news1", header: "Заголовок1", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit"),
+            CollectionItem(image: "news2", header: "Заголовок2", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit"),
+            CollectionItem(image: "news2", header: "Заголовок3", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit"),
+        ]
+        
+        let bannerItems = [
+            CollectionItem(image: "banner1"),
+            CollectionItem(image: "banner2"),
+            CollectionItem(image: "banner3"),
+        ]
+        
+        let storySection = CollectionSection(items: storyItems)
+        let messageSection = CollectionSection(items: messageItems)
+        let newsSection = CollectionSection(items: newsItems)
+        let bannerSection = CollectionSection(items: bannerItems)
+        
+        return [storySection, messageSection, newsSection, bannerSection]
+    }
+}
+
+
+struct CollectionItem: Identifiable {
+    let id: String = UUID().uuidString
+    let image: String
+    var header: String? = nil
+    var name: String? = nil
+    var text: String? = nil
+}
--- learnUIKit/ViewController.swift
+++ learnUIKit/ViewController.swift
@@ -1,50 +1,11 @@
-//
-//  ViewController.swift
-//  learnUIKit
-//
-//  Created by Максим Минаков on 14.11.2025.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         print("Hello")
-        
-        view.backgroundColor = .orange
-        // label
-        let label = UILabel()
-        label.text = "Hello world"
-        label.frame = CGRect(x: 100, y: 100, width: 300, height: 50)
-        label.textColor = .blue
-        label.font = .systemFont(ofSize: 50)
-        view.addSubview(label)
-        print(view.frame)
-        
-        // image
-        let someImageView = UIImageView()
-        someImageView.frame = CGRect(x: 100, y: 200, width: 300, height: 300)
-        someImageView.backgroundColor = .green
-        someImageView.image = UIImage(named: "img1")
-        someImageView.contentMode = .scaleAspectFill
-        view.addSubview(someImageView)
-        
-        // button
-        let action = UIAction { _ in
-            someImageView.frame = self.view.frame
-        }
-        
-        let button = UIButton(frame: CGRect(x: 50,
-                                            y: 600,
-                                            width: 200,
-                                            height: 80), primaryAction: action)
-        
-        button.setTitle("btn", for: .normal)
-        button.backgroundColor = .green
-        view.addSubview(button)
+        view.backgroundColor = .white
     }
 }
 
